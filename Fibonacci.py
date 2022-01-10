7ffd64b758a7c7445c2571be5c91afc00a679739
syphax94 <97119337+syphax94@users.noreply.github.com>
2022-01-10 22:50:03 +0100

    Update Fibonacci.py
    testt

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,17 @@
 # Calculate the Fibonacci Retracement Level Prices 
 
+import pandas as pd
+import pandas_datareader as web
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+
+
+tsla = yf.Ticker("TSLA")
+his = tsla.history(period="max")
+
+
 maximum_price = df['Close'].max()
 minimum_price = df['Close'].min()
 
@@ -9,31 +21,38 @@ second_level = maximum_price - difference  * 0.382
 third_level = maximum_price - difference * 0.5
 fourth_level = maximum_price - difference * 0.618
 
- # Print the price at each level
- print('Level Percentage Price($)')
- print('00.0%\t\t', maximum_price)
- print('23.6%\t\t', first_level)
- print('38.2%\t\t', second_level)
- print('50.0%\t\t', third_level)
- print('61.8%\t\t', fourth_level)
- print('100%\t\t', minimum_price) 
+
  
  # Plot the Fibonacci Level Prices along with the Close Price
 
-new_df = df
-plt.figure(figsize=(12,4))
-plt.title('Fibonacci Retracement Plot',fontsize=38)
+new_df =df
+fig = plt.figure(figsize=(20,10))
+
 plt.xlabel('Date', fontsize=28)
 plt.ylabel('Price (USD)', fontsize=28)
-
-
-plt.plot(new_df.index, new_df['Close'],color='black')
+ax = fig.add_subplot(1, 1, 1)
+plt.title('Fibonacci Retracement Plot',fontsize=38)  
+plt.plot(new_df.index, new_df['Close'], color='black')
 
 plt.axhline(maximum_price, linestyle='--' , alpha = 0.5, color='red')
+ax.fill_between(new_df.index, maximum_price, first_level,alpha = 0.5, color='red')
 plt.axhline(first_level, linestyle='--' , alpha = 0.5, color='orange')
+ax.fill_between(new_df.index, first_level, second_level, alpha = 0.5, color='orange')
 plt.axhline(second_level, linestyle='--' , alpha = 0.5, color='yellow')
+ax.fill_between(new_df.index, second_level, third_level, alpha = 0.5, color='yellow')
+
 plt.axhline(third_level, linestyle='--' , alpha = 0.5, color='green')
+ax.fill_between(new_df.index, third_level, fourth_level, alpha = 0.5, color='green')
+
 plt.axhline(fourth_level, linestyle='--' , alpha = 0.5, color='blue')
+ax.fill_between(new_df.index, fourth_level, minimum_price, alpha = 0.5, color='blue')
+
 plt.axhline(minimum_price, linestyle='--' , alpha = 0.5, color='purple')
+
+
+
+
+
+
 
 plt.show()
